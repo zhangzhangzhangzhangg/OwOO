ea657e9ba46cf46ba5b464ccfead40c871698d51
zhangzhangzhangzhangg <100786704+zhangzhangzhangzhangg@users.noreply.github.com>
2022-03-23 16:06:15 +0800

    Add files via upload

---
+++ OwOO帳號rinfo.php
@@ -0,0 +1,24 @@
+<body bgcolor=#d8b2d1>
+<center><h1>OwOO帳號資訊</h1>
+<?php
+$uname=$_GET["uname"];
+$umail=$_GET["umail"];
+$utele=$_GET["utele"];
+$uadd=$_GET["uadd"];
+$uid=$_GET["uid"];
+$usex=$_GET["usex"];
+
+echo "<b>你的姓名: </b>".$uname."先生/小姐<br/>";
+echo "<b>你的信箱: </b>".$umail."<br/>";
+echo "<b>你的電話: </b>".$utele."<br/>";
+echo "<b>你的地址: </b>".$uadd."<br/>";
+echo "<b>你的身分證字號: </b>".$uid."<br/>";
+if($usex=='1'){
+    echo"<b>你的性別:</b>男<br/>";
+}elseif($usex=='2'){
+    echo"<b>你的性別:</b>女<br/>";
+}else{
+    echo"<b>你的性別:</b>不明</br>";
+}
+
+?>
