4ba297da9218bbad28434fdd1952c240d82a131d
zhangzhangzhangzhangg <100786704+zhangzhangzhangzhangg@users.noreply.github.com>
2022-03-23 16:20:55 +0800

    Update OwOO帳號rinfo.php

--- OwOO帳號rinfo.php
+++ OwOO帳號rinfo.php
@@ -7,12 +7,15 @@ $utele=$_GET["utele"];
 $uadd=$_GET["uadd"];
 $uid=$_GET["uid"];
 $usex=$_GET["usex"];
+$uacc=$_GET["uacc"];
+$upw=$_GET["upw"];    
 
 echo "<b>你的姓名: </b>".$uname."先生/小姐<br/>";
 echo "<b>你的信箱: </b>".$umail."<br/>";
 echo "<b>你的電話: </b>".$utele."<br/>";
 echo "<b>你的地址: </b>".$uadd."<br/>";
 echo "<b>你的身分證字號: </b>".$uid."<br/>";
+    
 if($usex=='1'){
     echo"<b>你的性別:</b>男<br/>";
 }elseif($usex=='2'){
@@ -21,4 +24,7 @@ if($usex=='1'){
     echo"<b>你的性別:</b>不明</br>";
 }
 
+echo "<b>你的帳號: </b>".$uacc."<br/>";
+echo "<b>你的密碼: </b>".$upw."<br/>";
+    
 ?>
